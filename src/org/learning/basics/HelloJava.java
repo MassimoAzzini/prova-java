77de4b1f290a2ad67f98f12e893a1cd96c1454e9
MassimoAzzini <massimo.azzini.web@gmail.com>
2024-03-26 22:56:58 +0100

    prova commit

---
+++ src/org/learning/basics/HelloJava.java
@@ -0,0 +1,11 @@
+package org.learning.basics;
+
+public class HelloJava {
+    //shortcut MAIN
+    public static void main(String[] args) {
+        // shortcut SOUT
+        System.out.println("Hello Java!");
+        System.out.println(5+3);
+
+    }
+}
